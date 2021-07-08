fe02755ae1ff644049964df46dc29a3719b850e4
naderrazavi87 <nader.razavi87@gmail.com>
2021-07-07 22:49:31 -0700

    complete code

--- src/App.js
+++ src/App.js
@@ -5,40 +5,52 @@ import TodoList from './TodoList.js';
 import { v4 as uuidv4 } from 'uuid';
 
 
-const LOCAL_STORAGE_KEY = 'todoApp.todos' 
+const LOCAL_STORAGE_KEY = 'todoApp.todos'
 
 function App() {
-  const [todos, setTodos]=useState([ ])
-  const todoNameRef=useRef()
+  const [todos, setTodos] = useState([])
+  const todoNameRef = useRef()
 
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+    if (storedTodos) setTodos(storedTodos)
+  }, [])
 
-  useEffect(()=>{
+  useEffect(() => {
     localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
-  },[todos])
+  }, [todos])
 
+  function toggleTodo(id) {
+    const newTodos = [...todos]
+    const todo = newTodos.find(todo => todo.id === id)
+    todo.complete = !todo.complete
+    setTodos(newTodos)
+  }
 
+  function handleAddTodo(e) {
+    const name = todoNameRef.current.value
+    if (name === '') return
+    setTodos(prevTodos => {
+      return [...prevTodos, { id: uuidv4(), name: name, complete: false}]
+    })
+    todoNameRef.current.value = null
+  }
 
-
-
-function handleAddTodo(e){
-  const name= todoNameRef.current.value
-  if (name === '') return 
-    setTodos(prevTodos=>{
-      return [...prevTodos,{id:uuidv4(), name:name, complete:true}]
-})
-
-  todoNameRef.current.value=null
-}
+  function handleClearTodos() {
+    const newTodos = todos.filter(todo => !todo.complete)
+    setTodos(newTodos)
+  }
 
   return (
     <>
-   <TodoList todos={todos}/>
-   <input ref={todoNameRef} type="text"/>
-   <button onClick={handleAddTodo}>Add to do</button>
-   <button>Clear Completed to do</button>
-   <div>O left to do</div>
+      <TodoList todos={todos} toggleTodo={toggleTodo} />
+      <input ref={todoNameRef} type="text" />
+      <button onClick={handleAddTodo}>Add Todo</button>
+      <button onClick={handleClearTodos}>Clear Complete</button>
+      <div>{todos.filter(todo => !todo.complete).length} left to do</div>
     </>
-  );
+  )
 }
 
 export default App;
+
